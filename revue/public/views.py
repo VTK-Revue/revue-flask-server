0e79f926371a1eaa7154f744d0c50b30001428fd
MathijsC <mathijs.cuppens@hotmail.com>
2015-02-07 12:55:21 +0100

    Add WTForm implementation of login

--- revue/public/views.py
+++ revue/public/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, Blueprint, redirect, url_for, request, flash, session
 from revue.login import login_required
+from forms import LoginForm
 
 public_site = Blueprint('public', __name__, template_folder='templates')
 
@@ -10,21 +11,27 @@ def index():
 
 @public_site.route("/login", methods=['GET', 'POST'])
 def login():
+    form = LoginForm(request.form)
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            flash('Invalid login', 'danger')
-        else:
-            session['logged_in'] = True
-            flash('You just logged in', 'success')
-            return redirect('intern')
+        print "validate: "+str(form.validate())
+        if form.validate():
+            if form.username.data != 'admin' or form.password.data != 'admin':
+                print "wrong login"
+                flash('Invalid login', 'danger')
+            else:
+                print "login"
+                session['logged_in'] = True
+                flash('You just logged in', 'success')
+                return redirect('intern')
+        print form.username
 
-    return render_template("public/login.html")
+    return render_template("public/login.html", form=form)
 
 @public_site.route("/logout")
 @login_required
 def logout():
     session.pop('logged_in', None)
-    flash('You just logged lout', 'success')
+    flash('You just logged out', 'success')
     return redirect('')
 
 @public_site.route("/info")
