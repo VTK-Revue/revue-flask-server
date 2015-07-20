f2036f09afb9ebc65b61b2e56558752928c1f07d
Floris Kint <floris.kint@gmail.com>
2015-07-20 17:37:09 +0200

    Create public site stub

--- revue/public/views/__init__.py
+++ revue/public/views/__init__.py
@@ -1,73 +1,27 @@
-from flask import render_template, Blueprint, redirect, url_for, request, \
-    flash
-from revue.utilities.login import login_required
+from flask import render_template, Blueprint
+
 from forms import LoginForm, RegisterForm
-from revue.models import User, Registration
-from revue import db, bcrypt
-from revue.utilities import session
 
 public_site = Blueprint('public', __name__, template_folder='../templates')
 
 
 @public_site.route("/")
 @public_site.route("/index")
+@public_site.route("/home")
 def index():
-    return render_template("public/index.html")
+    return render_template("public/home.html")
 
 
-@public_site.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm(request.form)
-    if request.method == 'POST' and form.validate():
-            user = User.query.filter_by(username=form.username.data).first()
-            if user is not None and \
-                    bcrypt.check_password_hash(user.password,
-                                               form.password.data):
-                session.user_login(user)
-                flash('You just logged in', 'success')
-                return redirect('intern')
-            else:
-                flash('Invalid login', 'danger')
-
-    return render_template("public/login.html", form=form)
+@public_site.route("/ticket_info")
+def ticket_info():
+    return render_template("public/ticket_info.html")
 
 
-@public_site.route("/logout")
-@login_required
-def logout():
-    session.user_logout()
-    flash('You just logged out', 'success')
-    return redirect('')
+@public_site.route("/contact")
+def contact():
+    return render_template("public/contact.html")
 
 
-@public_site.route("/info")
-def info():
-    return render_template("public/info.html")
-
-
-@public_site.route("/archive")
-def archive():
-    return render_template("public/archive.html")
-
-
-@public_site.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
-        if User.query.filter_by(username=form.username.data).first() is None:
-            newUser = User(
-                form.firstName.data,
-                form.lastName.data,
-                form.email.data,
-                form.username.data,
-                form.password.data
-            )
-            #TODO: check if duplicate username should be checked
-            db.session.add(Registration.from_user(newUser))
-            db.session.commit()
-            #TODO: send notification e-mail to activate this account
-            flash('You just created an account. Once your account has been activated, you\''
-                  'll be able to access the internal part of website.', 'success')
-            return redirect('/')
-        flash('A user with this username already exists, choose another one')
-    return render_template("public/register.html", form=form)
+@public_site.route("/voorstellingen")
+def voorstellingen():
+    return render_template("public/voorstellingen.html")
---
+++ revue/public/views/login/__init__.py
@@ -0,0 +1,55 @@
+__author__ = 'Floris'
+from revue.public.views import public_site
+from revue.public.views import LoginForm, RegisterForm
+from revue.utilities.login import login_required
+from revue.utilities import session
+from revue.models import User, Registration
+from flask import request, render_template, flash, redirect
+from revue import bcrypt, db
+
+@public_site.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user is not None and \
+                    bcrypt.check_password_hash(user.password,
+                                               form.password.data):
+                session.user_login(user)
+                flash('You just logged in', 'success')
+                return redirect('intern')
+            else:
+                flash('Invalid login', 'danger')
+
+    return render_template("public/login/login.html", form=form)
+
+
+@public_site.route("/logout")
+@login_required
+def logout():
+    session.user_logout()
+    flash('You just logged out', 'success')
+    return redirect('/')
+
+
+@public_site.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        if User.query.filter_by(username=form.username.data).first() is None:
+            newUser = User(
+                form.firstName.data,
+                form.lastName.data,
+                form.email.data,
+                form.username.data,
+                form.password.data
+            )
+            #TODO: check if duplicate username should be checked
+            db.session.add(Registration.from_user(newUser))
+            db.session.commit()
+            #TODO: send notification e-mail to activate this account
+            flash('You just created an account. Once your account has been activated, you\''
+                  'll be able to access the internal part of website.', 'success')
+            return redirect('/')
+        flash('A user with this username already exists, choose another one')
+    return render_template("public/login/register.html", form=form)
