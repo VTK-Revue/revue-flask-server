41128829aa59279899bea8a897d619842443d66b
Floris Kint <floris.kint@gmail.com>
2016-09-24 10:49:19 +0200

    Remove non-existing public views

--- revue/public/views/__init__.py
+++ revue/public/views/__init__.py
@@ -10,18 +10,3 @@ from . import login
 @public_site.route("/home")
 def index():
     return render_template("public/home.html")
-
-
-@public_site.route("/ticket_info")
-def ticket_info():
-    return render_template("public/ticket_info.html")
-
-
-@public_site.route("/contact")
-def contact():
-    return render_template("public/contact.html")
-
-
-@public_site.route("/voorstellingen")
-def voorstellingen():
-    return render_template("public/voorstellingen.html")
